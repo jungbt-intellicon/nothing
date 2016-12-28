d53f072dafe4843ce600ed296f2b93e8b181810d
jungbt <jungbt82@gmail.com>
2016-12-28 11:52:16 +0900

    수정

--- testJava2/src/testJava2/JavaClass1.java
+++ testJava2/src/testJava2/JavaClass1.java
@@ -4,5 +4,7 @@ public class JavaClass1 {
 	public static void main(String[] args) {
 		System.out.println("새로시작하기 ");
 System.out.println("branch2 commit");
+
+System.out.println("branch2 다시 수정");
 	}
 }

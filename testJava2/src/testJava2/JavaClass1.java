6b739629d785ef86ded8478ad1676c460d9cc49d
jungbt <jungbt82@gmail.com>
2016-12-28 12:06:13 +0900

    branch1 다시 변경

--- testJava2/src/testJava2/JavaClass1.java
+++ testJava2/src/testJava2/JavaClass1.java
@@ -4,5 +4,6 @@ public class JavaClass1 {
 	public static void main(String[] args) {
 		System.out.println("새로시작하기 ");
 		System.out.println("branch1 변경");
+		System.out.println("branch1 다시 변경");
 	}
 }

78cbd72dbc86a20a80579fe71e935c3ca87855d8
jungbt <jungbt82@gmail.com>
2016-12-28 11:39:46 +0900

    branch1 commit

--- testJava2/src/testJava2/JavaClass1.java
+++ testJava2/src/testJava2/JavaClass1.java
@@ -3,6 +3,9 @@ package testJava2;
 public class JavaClass1 {
 	public static void main(String[] args) {
 		System.out.println("새로시작하기 ");
+		System.out.println("branch1 commit");
+		
+		
 		System.out.println("master 변경 for rebase");
 
 	}

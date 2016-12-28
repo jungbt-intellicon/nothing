3f5a2363664203461977f6f5ad6703a270721a37
jungbt <jungbt82@gmail.com>
2016-12-28 12:04:40 +0900

    리셋

--- testJava2/src/testJava2/JavaClass1.java
+++ testJava2/src/testJava2/JavaClass1.java
@@ -3,26 +3,5 @@ package testJava2;
 public class JavaClass1 {
 	public static void main(String[] args) {
 		System.out.println("새로시작하기 ");
-		System.out.println("branch1 commit");
-		
-		
-		System.out.println("master 변경 for rebase");
-		
-		System.out.println("rebase 후 merge");
-		
-		System.out.println("branch2 머지후 branch1 머지");
-
-System.out.println("branch2 commit");
-
-System.out.println("conflict 후 수정");
-System.out.println("branch2 다시 수정");
-
-System.out.println("rebase를 위한 수정");
-
-System.out.println("branch2를 위한 수정");
-
-System.out.println("branch1 에서 branch2를 위한 수정");
-
-System.out.println("branch1 과 관계없는 수정");
 	}
 }

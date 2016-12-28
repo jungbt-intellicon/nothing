0555a003055e7b7ae22365826adfac8040cd404c
jungbt <jungbt82@gmail.com>
2016-12-28 11:37:45 +0900

    master 1 commit

--- testJava2/src/testJava2/JavaClass1.java
+++ testJava2/src/testJava2/JavaClass1.java
@@ -2,18 +2,7 @@ package testJava2;
 
 public class JavaClass1 {
 	public static void main(String[] args) {
-		System.out.println("hello world");
-		
-		System.out.println("hello world2");
-		
-		System.out.println("modified for branch1");
-		
-		System.out.println("change for master");
-		
-		System.out.println("branch2 생성");
-		
-		System.out.println("master 변경");
-		
-		System.out.println("master 변경2");
+		System.out.println("새로시작하기 ");
+
 	}
 }
